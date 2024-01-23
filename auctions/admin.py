ddbbd077350327bdfb7e6a9764eed8feb460fb01
zsoltadam92 <zsoltadam92@gmail.com>
2024-01-23 20:00:13 +0100

    add makemigrations

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import User, AuctionListing, Bid, Comment
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(AuctionListing)
+admin.site.register(Bid)
+admin.site.register(Comment)
