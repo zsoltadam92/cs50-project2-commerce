fb9198925a1fab584a485f10e763b35b82ad809c
zsoltadam92 <zsoltadam92@gmail.com>
2024-02-05 17:06:36 +0100

    listing deatails style

--- auctions/views.py
+++ auctions/views.py
@@ -15,11 +15,11 @@ class NewListing(forms.Form):
     title = forms.CharField(
         label="",
         max_length=200,
-        widget=forms.TextInput(attrs={'placeholder': 'Title', 'class': 'form-control form-group col-6'})
+        widget=forms.TextInput(attrs={'placeholder': 'Title', 'class': 'form-control form-group col-12'})
     )
     description = forms.CharField(
         label="",
-        widget=forms.Textarea(attrs={'placeholder': 'Description', 'class': 'form-control form-group col-6'})
+        widget=forms.Textarea(attrs={'placeholder': 'Description', 'class': 'form-control form-group col-12'})
     )
     starting_bid = forms.DecimalField(
         label="",
@@ -27,11 +27,11 @@ class NewListing(forms.Form):
         decimal_places=2,
         min_value=0.01,
         max_value=9999.99,
-        widget=forms.NumberInput(attrs={'placeholder': 'Starting bid', 'class': 'form-control form-group col-6'})
+        widget=forms.NumberInput(attrs={'placeholder': 'Starting bid', 'class': 'form-control form-group col-12'})
     )
     image_url = forms.URLField(
         label="",
-        widget=forms.URLInput(attrs={'placeholder': 'Image URL', 'class': 'form-control form-group col-6'})
+        widget=forms.URLInput(attrs={'placeholder': 'Image URL', 'class': 'form-control form-group col-12'})
     )
     category = forms.CharField(
         label="",
@@ -46,7 +46,7 @@ class AddBid(forms.Form):
 class AddComment(forms.Form):
     comment = forms.CharField(
         label="",
-        widget=forms.Textarea(attrs={'placeholder': 'Write comment', 'class': 'form-control form-group col-6'})
+        widget=forms.Textarea(attrs={'placeholder': 'Write comment', 'class': 'form-control form-group'})
     )
 
 def index(request):
